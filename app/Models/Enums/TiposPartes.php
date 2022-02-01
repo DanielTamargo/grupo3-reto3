faf3ae5cbd7303fc8a178be482d4a2bb7905975c
TxaberGardeazabal <txaber.musico@gmail.com>
2022-02-01 13:50:50 +0100

    mostrar tareas tecnicos + validar usuario

---
+++ app/Models/Enums/TiposPartes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Enums;
+
+enum TiposPartes: string {
+    case INCIDENCIA = "incidencia";
+    case AVERIA = "averia";
+    case REVISION = "revision";
+}
+
+?>
--- resources/views/layouts/tecnico.blade.php
+++ resources/views/layouts/tecnico.blade.php
@@ -41,7 +41,7 @@
         <div id="top-nav-circle" class="bg-success rounded-circle"></div>
         <nav class="text-center bg-success d-flex flex-column justify-content-around" id="fixednav">
             <div>
-                <a href="{{ route('tecnico.create') }}" class="btn btn-lg p-1 text-white border border-2 border-light">
+                <a href="{{ route('tecnico.create', ['cod' => $codUsr]) }}" class="btn btn-lg p-1 text-white border border-2 border-light">
                     <!-- Clipboard check -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
@@ -51,7 +51,7 @@
                 </a>
             </div>
             <div>
-                <a href="{{ route('tecnico.show') }}" class="btn btn-lg p-1 text-white border border-2 border-light">
+                <a href="{{ route('tecnico.show', ['cod' => $codUsr]) }}" class="btn btn-lg p-1 text-white border border-2 border-light">
                     <!-- Clipboard plus -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/>
@@ -61,7 +61,7 @@
                 </a>
             </div>
             <div>
-                <a href="{{ route('tecnico.historial') }}" class="btn btn-lg p-1 text-white border border-2 border-light">
+                <a href="{{ route('tecnico.historial', ['cod' => $codUsr]) }}" class="btn btn-lg p-1 text-white border border-2 border-light">
                     <!-- Clock history -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
                     <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
@@ -71,7 +71,7 @@
                 </a>
             </div>
             <div>
-                <a href="{{ route('tecnico.manual') }}" class="btn btn-lg p-1 text-white border border-2 border-light">
+                <a href="{{ route('tecnico.manual', ['cod' => $codUsr]) }}" class="btn btn-lg p-1 text-white border border-2 border-light">
                     <!-- Journal text -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-journal-text" viewBox="0 0 16 16">
                     <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
@@ -81,7 +81,7 @@
                 </a>
             </div>
             <div>
-                <a href="{{ route('tecnico.piezas') }}" class="btn btn-lg p-1 text-white border border-2 border-light">
+                <a href="{{ route('tecnico.piezas', ['cod' => $codUsr]) }}" class="btn btn-lg p-1 text-white border border-2 border-light">
                     <!-- Journal text -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
                         <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.356 3.356a1 1 0 0 0 1.414 0l1.586-1.586a1 1 0 0 0 0-1.414l-3.356-3.356a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0zm9.646 10.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708zM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11z"/>
@@ -89,7 +89,7 @@
                 </a>
             </div>
             <div>
-                <a href="{{ route('tecnico.home') }}" class="btn btn-lg p-1 text-white border border-2 border-light">
+                <a href="{{ route('tecnico.home', ['cod' => $codUsr]) }}" class="btn btn-lg p-1 text-white border border-2 border-light">
                     <!-- House -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
