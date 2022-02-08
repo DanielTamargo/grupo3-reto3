6e29abed016fdeac7baeec5cf700a3b4c3167153
ALAITZ CANDELA MURELAGA <alaitz.candela@ikasle.egibide.org>
2022-02-08 20:18:13 +0100

    merge develop

--- app/Http/Controllers/Estadisticas.php
+++ app/Http/Controllers/Estadisticas.php
@@ -6,12 +6,19 @@ use Illuminate\Http\Request;
 use App\Models\Ascensor;
 use App\Models\Tecnico;
 use App\Models\Tarea;
+use Illuminate\Support\Facades\Auth;
 
 class Estadisticas extends Controller
 {
     public function mostrar(Request $request) {
-        // TODO validar que el usuario tiene permisos (admin y jefeequipo)
-
-        return view('estadisticas');
+        // Comprobamos que el usuario es administrador o jefe de equipo y si es le llevamos a la vista y sino
+        // le llevamos al home
+        if(Auth::user()->rol == 'administrador' || Auth::user()->rol == 'jefeequipo'){
+            return view('estadisticas');
+        }
+        else{
+            return view('home');
+        }
+        
     }
 }
